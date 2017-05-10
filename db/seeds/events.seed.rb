ac5664846b4a8036f356fcbc37750ecee2b55d94
Jonjoe Whitfield <me@jonjoe.io>
2017-05-10 16:54:31 +0100

    seeding bugs

--- db/seeds/events.seed.rb
+++ db/seeds/events.seed.rb
@@ -1,6 +1,6 @@
 15.times do
 	Event.create({
-		date: Faker::Date.between(Date.today, (Date.today + 30.days))
+		date: Faker::Date.between(Date.today, (Date.today + 30.days)),
 		timeslot: 1,
 		name: Faker::Lorem.sentence,
 		description: Faker::Lorem.sentence
